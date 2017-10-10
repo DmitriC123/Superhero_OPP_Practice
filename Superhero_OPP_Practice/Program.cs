699842b81b11b2ca245ccc0e2187977347f1e0d5
DmitriC123 <cohan.dima@gmail.com>
2017-10-09 21:48:03 -0400

    initial commit

---
+++ Superhero_OPP_Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_OPP_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhero firstOne = new Superhero();
+
+            firstOne.StrengthLevel = "Strong as hell ";
+            firstOne.HasCape = "When it doesn't fly away. ";
+            firstOne.IsKnown = "Famous as ever. ";
+            firstOne.HasMoney = 9;
+
+
+
+
+    }
+}
---
+++ Superhero_OPP_Practice/Superhero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_OPP_Practice
+{
+    class Superhero
+    {
+        private string strengthLevel;
+        private int hasMoney;
+        private string hasCape;
+        private string isKnown;
+
+        public string StrengthLevel
+        {
+            get { return this.strengthLevel; }
+            set { this.strengthLevel = value; }
+        }
+        public int HasMoney
+        {
+            get { return this.hasMoney; }
+            set { this.hasMoney = value; }
+        }
+        public string HasCape
+        {
+            get { return this.hasCape; }
+            set { this.hasCape = value; }
+        }
+        public string IsKnown
+        {
+            get { return this.isKnown; }
+        }
+
+        public Superhero()
+        {
+
+        }
+        public Superhero(int hasMoney, string isKnown, string strengthLevel)
+        {
+            this.hasMoney = hasMoney;
+            this.isKnown = isKnown;
+            this.strengthLevel = strengthLevel;
+        }
+        public void isHit()
+        {
+            strengthLevel = "Damaged. ";
+            Console.WriteLine(strengthLevel);
+        }
+        public void lostCape()
+        {
+            hasCape = "Oh no! It's gone! ";
+            Console.WriteLine(hasCape);
+        }
+    }
+}
